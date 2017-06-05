cfcddb855fc739f81900fe68dd405eafd1d2db68
Laurent M <laurent.montigny@gmail.com>
2017-06-05 14:03:23 +0200

    add implementation folder

---
+++ Challenges/Implementation/angry-professor.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    for(int a0 = 0; a0 < t; a0++){
+        int n;
+        int k;
+        cin >> n >> k;
+        vector<int> a(n);
+        int sum=0;
+        for(int a_i = 0;a_i < n;a_i++){
+            cin >> a[a_i];
+            if(a[a_i]<=0) sum++;
+        }
+        if (sum<k) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+    return 0;
+}
