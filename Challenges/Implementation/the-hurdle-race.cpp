1c69766a7e777f06781afd096d235d81d5274a65
Laurent M <laurent.montigny@gmail.com>
2017-06-25 13:13:00 +0200

    new challenge

---
+++ Challenges/Implementation/the-hurdle-race.cpp
@@ -0,0 +1,45 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    int k;
+    cin >> n >> k;
+    vector<int> height(n);
+    for(int height_i = 0; height_i < n; height_i++){
+       cin >> height[height_i];
+    }
+    
+    // your code goes here
+    int height_max = *std::max_element(height.begin(), height.end());
+    int diff = k-height_max;
+    
+    cout << (diff<0? abs(diff) : 0);
+    //if(diff < 0){cout << abs(diff);}
+    //else {cout << 0;}
+
+    return 0;
+}
