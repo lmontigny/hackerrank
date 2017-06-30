fadfb9e0a4c07480f08e755c57692a07b7c67178
Laurent Montigny <lmontigny@users.noreply.github.com>
2017-06-30 15:18:34 +0200

    Create jumping-on-the-clouds-revisited.cpp

---
+++ Challenges/Implementation/jumping-on-the-clouds-revisited.cpp
@@ -0,0 +1,41 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    int k;
+    cin >> n >> k;
+    vector<int> c(n);
+    for(int c_i = 0;c_i < n;c_i++){
+       cin >> c[c_i];
+    }
+    
+    int E=100;
+    for(int i = 0; i < n; i+=k) E-=(1+2*c[i]);
+    cout <<  E << endl;
+
+    return 0;
+}
